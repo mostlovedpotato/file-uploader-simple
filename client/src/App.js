b378aaad6286eb012ccf333a594bd5436abae8a8
Pico <domferrelu@gmail.com>
2020-12-19 12:04:56 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container mt-4">
       <h4 className="display-4 text-center md-4">
-      <i className="	fa fa-files-o"></i>React File Upload</h4>
+      <i className="	fa fa-file"></i>React File Upload</h4>
       <Fileupload/>
     </div>
   );
--- client/src/components/Fileupload.js
+++ client/src/components/Fileupload.js
@@ -1,11 +1,14 @@
-import React,{useState} from 'react'
-import './xd.css'
+import React,{useState} from 'react';
+import './xd.css';
+import Message from './Message';
 import axios from 'axios';
 
 export const Fileupload = () => {
     const [file,setFile] = useState('');
     const [filename,setFilename]= useState('Choose File');
-    
+    const [uploadedFile,setUploadedFile] = useState({});
+    const [message,setMessage] = useState('');
+
     const onChange=(e)=>{
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name)
@@ -19,18 +22,25 @@ export const Fileupload = () => {
         try {
             const res = await axios.post('/upload',formData,{
                 headers:{
-                    'Content-type':'multipart/form-data'
+                    'Content-Type':'multipart/form-data'
                 }
             })
             const {fileName,filePath} = res.data;
+            setUploadedFile({fileName,filePath});
+            setMessage("File Uploaded");
             
         } catch (error) {
-            
+            if(error.response.status===500) {
+                setMessage("There is a problem in b/w");
+            }else{
+                setMessage(error.response.data.msg);
+            }
         }
     }
 
     return (
         <>
+            {message ? <Message msg={message} /> : null}
           <form onSubmit={onSubmit}>
             <div className="custom-file mb-4">
                 <input
@@ -46,8 +56,16 @@ export const Fileupload = () => {
             type="submit"
             value="Upload"
             className="btn btn-primary mt-4"
-            ></button>
+            >Upload</button>
           </form>
+          {uploadedFile ? (
+              <div className="row mt-5">
+                  <div>
+                      <h3 className="text-center">{uploadedFile.fileName}</h3>
+                      <img style={{width:'100%'}} src={uploadedFile.filePath}></img>
+                  </div>
+              </div>
+          ):null}
         </>
     )
 }
